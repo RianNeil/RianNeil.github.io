79c2e85ea744d354105308a5a6301be7426f3a49
rianneil <rianneildesigns@gmail.com>
2017-03-24 17:58:07 -0400

    organize css and js files by moving to external files.

--- parcelcast/html/js/main.js
+++ parcelcast/html/js/main.js
@@ -39,3 +39,23 @@ $('.the-carousel').on('afterChange', function(){
 document.getElementById( 'carousel-slider' ).addEventListener("input", function() {
 	$('.the-carousel').slick( 'slickGoTo', $( "#carousel-slider" ).val(), true );
 });
+
+
+
+$(document).ready(function() {
+	$('.popup-wrap').removeClass('hidden').addClass('visible');
+
+	$('.popup-btn').click(function(e) {
+    $('.popup-wrap').fadeIn(250);
+    $('.popup-box').removeClass('transform-out').addClass('transform-in');
+
+    e.preventDefault();
+  });
+
+  $('.popup-close').click(function(e) {
+    $('.popup-wrap').fadeOut(500);
+    $('.popup-box').removeClass('transform-in').addClass('transform-out');
+
+    e.preventDefault();
+  });
+});
